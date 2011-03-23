37aed37645b819dbf0fecca66c2bf28c4c17d3ee
Dave Hayes <dwhayes@gmail.com>
2011-03-23 15:54:32 -0500

    Try using yajl instead of demjson

--- bench.py
+++ bench.py
@@ -2,7 +2,7 @@
 
 import time
 import anyjson
-import demjson
+#import demjson
 
 if __name__ == "__main__":
     print "Reading JSON data file into memory:",
@@ -11,18 +11,21 @@ if __name__ == "__main__":
         data = data_file.read()
     finish_time = time.time()
     print "Read %0.2fMiB in %0.2f seconds." % (len(data) / 1024.0 / 1024.0, finish_time - start_time)
-    print "Creating Python object from JSON data using demjson:",
-    start_time = time.time()
-    python_obj = demjson.decode(data)
-    finish_time = time.time()
-    print "Finished in %0.2f seconds." % (finish_time - start_time)
-    print "Creating JSON data from Python object using demjson:",
-    start_time = time.time()
-    json_string = demjson.encode(python_obj, strict=True)
-    finish_time = time.time()
-    print "Finished in %0.2f seconds." % (finish_time - start_time)
-    del data
-    print "Beginning main benchmarks at 10 iterations per operation:\n"
+    anyjson.force_implementation('yajl')
+    python_obj = anyjson.deserialize(data)
+    json_string = anyjson.serialize(python_obj)
+#    print "Creating Python object from JSON data using demjson:",
+#    start_time = time.time()
+#    python_obj = demjson.decode(data)
+#    finish_time = time.time()
+#    print "Finished in %0.2f seconds." % (finish_time - start_time)
+#    print "Creating JSON data from Python object using demjson:",
+#    start_time = time.time()
+#    json_string = demjson.encode(python_obj, strict=True)
+#    finish_time = time.time()
+#    print "Finished in %0.2f seconds." % (finish_time - start_time)
+#    del data
+#    print "Beginning main benchmarks at 10 iterations per operation:\n"
     for implementation in anyjson._modules:
         name = implementation[0]
 #        if name == 'cjson':
