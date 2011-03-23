c2f2b1500fa18463bbef55a5a2c70c68262eab1d
Dave Hayes <dwhayes@gmail.com>
2011-03-23 12:44:59 -0500

    Adds some nicer output

--- bench.py
+++ bench.py
@@ -8,11 +8,22 @@ if __name__ == "__main__":
     libraries = {}
     with open("data.json", "r") as data_file:
         data = data_file.read()
+    print "Creating Python object from JSON data using demjson:",
+    start_time = time.time()
     python_obj = demjson.decode(data)
+    finish_time = time.time()
+    print "Finished in %0.2f seconds." % (finish_time - start_time)
+    print "Creating JSON data from Python object using demjson:",
+    start_time = time.time()
     json_string = demjson.encode(python_obj)
+    finish_time = time.time()
+    print "Finished in %0.2f seconds." % (finish_time - start_time)
     del data
+    print "Beginning main benchmarks at 14 iterations per operation:\n"
     for implementation in anyjson._modules:
         name = implementation[0]
+        if name == 'cjson':
+            continue
         try:
             anyjson.force_implementation(name)
         except:
@@ -20,18 +31,20 @@ if __name__ == "__main__":
         for func, data, action in [(anyjson.serialize, python_obj, 'encode'),
                                   (anyjson.deserialize, json_string, 'decode')]:
             runs = []
+            method = "%s %s:" % (name, action)
+            print "%s\t" % method.ljust(20),
+            print "",
             try:
-                for i in range(10):
+                for i in range(14):
+                    print "\b.",
                     start_time = time.time()
                     func(data)
                     finish_time = time.time()
                     runs.append(finish_time - start_time)
             except:
                 del runs
-                method = "%s %s:" % (name, action)
-                print "%s\tERROR" % method.ljust(20)
+                print "ERROR"
                 continue
             average = float(sum(runs) / len(runs))
             del runs
-            method = "%s %s:" % (name, action)
-            print "%s\t%f seconds" % (method.ljust(20), average)
+            print "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b%0.4f seconds" % average
